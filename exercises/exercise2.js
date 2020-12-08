fd25d0c9e848f91d76f88f8404094dffb2e54a17
Kevin Meldau <me@kevinmeldau.com>
2020-12-07 23:36:01 -0500

    Stared working on Slackbot

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -1,15 +1,20 @@
 // Set this assignment to true if you do want to use it.
-module.exports.ACTIVATE_BOT = false;
+module.exports.ACTIVATE_BOT = true;
 
 module.exports.botScripts = [
   // Write an interaction where one user says "I'm tired", the bot says "Wake up!"
   //
   // mychatbot> I'm tired
   // mychatbot responds "Wake up!"
-  
-
-
-
+  {
+    label: 'Question 1 - tired',
+    prompt: 'I\'m tired',
+    handler: function () {
+      return 'Well go put on your big boy pants, grab some coffee, and wake up!';
+    },
+    isCaseSensitive: false,
+    isListening: true,
+  },
 
   // Write an interaction where one user asks the bot 
   // "What's the area of a circle with radius X?" where X is any number
@@ -17,9 +22,14 @@ module.exports.botScripts = [
   // 
   // mychatbot> mychatbot What's the area of a circle with radius 5?
   // mychatbot responds to user "The area is 78.5398...."
-  
-
-
+  {
+    label: 'An Example Script with Variables',
+    prompt: 'Say hello to #{name}',
+    handler: function (params) {
+      return 'Hello ' + params.name + '!';
+    },
+    isListening: true,
+  },
 
 
   // Write an interaction where one user asks the bot 
