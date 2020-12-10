6fc218305ae5575574878da3b0d7d2c51e8c2d9e
Kevin Meldau <me@kevinmeldau.com>
2020-12-10 14:02:16 -0500

    Added in state to all questions

--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -10,15 +10,25 @@ module.exports.botScripts = [
     },
     isListening: true,
   },
+
+  // handler: function (params, state) {
+  //   state.firstName = params.name
+  // }
+
+  // handler: function (params, state) {
+  //   return 'Hi ' + state.firstName
+  // }
+
   {
     label: "Name",
     prompt: "It's #{name}",
-    handler: function (params) {
-      const firstName = params.name;
+    handler: function (params, state) {
+      let firstName = params.name;
+      state.firstName = params.name;
       return (
-        "Great " +
-        firstName +
-        "! Now let's find out a bit more about you. What state are you from?"
+        "OK, " +
+        state.firstName +
+        " now let's find out a bit more about you. What state are you from?"
       );
     },
     isListening: true,
@@ -108,10 +118,13 @@ module.exports.botScripts = [
   {
     label: "Question 1",
     prompt: "Q1: #{answer1}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer1 = params.answer1;
       if (answer1.toLowerCase() === "geography") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer1 +
           " is the correct answer. Well done! \n Next question: What is the second biggest country in the world?"
         );
@@ -124,10 +137,13 @@ module.exports.botScripts = [
   {
     label: "Question 2",
     prompt: "Q2: #{answer2}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer2 = params.answer2;
       if (answer2.toLowerCase() === "canada") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer2 +
           " is the correct answer. Well done! \n Next question: What is the longest river in Africa?"
         );
@@ -140,10 +156,13 @@ module.exports.botScripts = [
   {
     label: "Question 3",
     prompt: "Q3: #{answer3}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer3 = params.answer3;
       if (answer3.toLowerCase() === "nile") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           "The " +
           answer3 +
           " is the correct answer. Well done! \n Next question: Mumbai, Chennai, and Kolkata are metropolitan cities in which country?"
@@ -157,10 +176,13 @@ module.exports.botScripts = [
   {
     label: "Question 4",
     prompt: "Q4: #{answer4}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer4 = params.answer4;
       if (answer4.toLowerCase() === "india") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer4 +
           " is the correct answer. Well done! \n Next question: What country calls itself Nippon?"
         );
@@ -173,10 +195,13 @@ module.exports.botScripts = [
   {
     label: "Question 5",
     prompt: "Q5: #{answer5}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer5 = params.answer5;
       if (answer5.toLowerCase() === "japan") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer5 +
           " is the correct answer. Well done! \n Next question: Which countries share the longest border in the world?"
         );
@@ -189,11 +214,14 @@ module.exports.botScripts = [
   {
     label: "Question 6",
     prompt: "Q6: #{answer6}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer6 = params.answer6;
       if (answer6.toLowerCase() === "usa and canada") {
         return (
-          "The " +
+          "Nice job " +
+          state.firstName +
+          ", " +
+          "the " +
           answer6 +
           " is the correct answer. Well done! \n Next question: What is the line of latitude that runs around the center of the world called?"
         );
@@ -206,11 +234,14 @@ module.exports.botScripts = [
   {
     label: "Question 7",
     prompt: "Q7: #{answer7}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer7 = params.answer7;
       if (answer7.toLowerCase() === "equator") {
         return (
-          "The " +
+          "Nice job " +
+          state.firstName +
+          ", " +
+          "the " +
           answer7 +
           " is the correct answer. Well done! \n Next question: What is the world’s second highest mountain after Everest in Asia?"
         );
@@ -223,10 +254,13 @@ module.exports.botScripts = [
   {
     label: "Question 8",
     prompt: "Q8: #{answer8}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer8 = params.answer8;
       if (answer8.toLowerCase() === "k2") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer8 +
           " is the correct answer. Well done! \n Next question: In which ocean is Fiji?"
         );
@@ -239,11 +273,14 @@ module.exports.botScripts = [
   {
     label: "Question 9",
     prompt: "Q9: #{answer9}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer9 = params.answer9;
       if (answer9.toLowerCase() === "pacific") {
         return (
-          "The " +
+          "Nice job " +
+          state.firstName +
+          ", " +
+          "the " +
           answer9 +
           " is the correct answer. Well done! \n Next question: Which river rises in Peru, has no bridge crossing it and enters the sea in Brazil?"
         );
@@ -256,11 +293,14 @@ module.exports.botScripts = [
   {
     label: "Question 10",
     prompt: "Q10: #{answer10}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer10 = params.answer10;
       if (answer10.toLowerCase() === "amazon") {
         return (
-          "The " +
+          "Nice job " +
+          state.firstName +
+          ", " +
+          "the " +
           answer10 +
           " is the correct answer. Well done! Clearly, these are too easy for you. The questions are about to get a lot harder. \n Next question: What was the name of the supercontinent of 200 million years ago?"
         );
@@ -273,10 +313,13 @@ module.exports.botScripts = [
   {
     label: "Question 11",
     prompt: "Q11: #{answer11}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer11 = params.answer11;
       if (answer11.toLowerCase() === "pangaea") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer11 +
           " is the correct answer. Well done! \n Next question: What is the largest of the Channel Islands?"
         );
@@ -289,10 +332,13 @@ module.exports.botScripts = [
   {
     label: "Question 12",
     prompt: "Q12: #{answer12}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer12 = params.answer12;
       if (answer12.toLowerCase() === "jersey") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer12 +
           " is the correct answer. Well done! \n Next question: Which country shares the same peninsula as Spain?"
         );
@@ -305,10 +351,13 @@ module.exports.botScripts = [
   {
     label: "Question 13",
     prompt: "Q13: #{answer13}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer13 = params.answer13;
       if (answer13.toLowerCase() === "portugal") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer13 +
           " is the correct answer. Well done! \n Next question: The United Kingdom consists of England, Scotland, Wales, and which other region?"
         );
@@ -321,10 +370,13 @@ module.exports.botScripts = [
   {
     label: "Question 14",
     prompt: "Q14: #{answer14}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer14 = params.answer14;
       if (answer14.toLowerCase() === "northern ireland") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer14 +
           " is the correct answer. Well done! \n Next question: Which country is called the Land of Rising Sun?"
         );
@@ -337,10 +389,13 @@ module.exports.botScripts = [
   {
     label: "Question 15",
     prompt: "Q15: #{answer15}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer15 = params.answer15;
       if (answer15.toLowerCase() === "japan") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer15 +
           " is the correct answer. Well done! \n The next question is the final question, it's hard. \n Next question: What is the capital city of Slovenia, formerly part of the state of Yugoslavia?"
         );
@@ -353,10 +408,13 @@ module.exports.botScripts = [
   {
     label: "Final Answer",
     prompt: "Q16: #{answer16}",
-    handler: function (params) {
+    handler: function (params, state) {
       const answer16 = params.answer16;
       if (answer16.toLowerCase() === "ljubljana") {
         return (
+          "Nice job " +
+          state.firstName +
+          ", " +
           answer16 +
           " is the correct answer. Well done! \n Your Geography skills are on point. Go off and teach this stuff instead of trying to punch code."
         );
